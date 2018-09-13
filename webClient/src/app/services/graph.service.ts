6acfe119609acd4eda9191920389694188591f15
dprizentsov <dprizentsov@rocketsoftware.com>
2018-09-13 09:31:53 -0400

    call remote gremlin service

--- webClient/src/app/services/graph.service.ts
+++ webClient/src/app/services/graph.service.ts
@@ -1,21 +1,9 @@
-
-
-/*
-  This program and the accompanying materials are
-  made available under the terms of the Eclipse Public License v2.0 which accompanies
-  this distribution, and is available at https://www.eclipse.org/legal/epl-v20.html
-  
-  SPDX-License-Identifier: EPL-2.0
-  
-  Copyright Contributors to the Zowe Project.
-*/
-
 import { Injectable } from '@angular/core';
 import { Http} from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class HelloService {
+export class GraphService {
   private destination:string;
   
   constructor(private http: Http){}
@@ -24,13 +12,8 @@ export class HelloService {
     this.destination = path;
   }
 
-  sayHello(text: string): Observable<any> {
-    const requestBody = {
-      "_objectType": "com.rs.mvd.sampleapp.request.hello",
-      "_metaDataVersion": "1.0.0",
-      "messageFromClient": text
-    }
-    return this.http.post(this.destination, requestBody);
+  executeGremlin(query: string): Observable<any> {
+    return this.http.post(this.destination, query);
   }
 
 }
