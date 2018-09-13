0a9e6364df8e2934a6ca481dd123ccd55310e829
dprizentsov <dprizentsov@rocketsoftware.com>
2018-09-13 15:29:53 -0400

    discoveryForm

---
+++ webClient/src/app/discoveryForm/discoveryForm.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit , Inject, Input } from '@angular/core';
+import { Angular2InjectionTokens } from 'pluginlib/inject-resources';
+
+import { GraphService } from '../services/graph.service';
+
+@Component({
+  selector: 'discovery-form',
+  templateUrl: './discoveryForm.component.html',
+  styleUrls: ['./discoveryForm.component.css', './discoveryForm.component.custom.css']
+})
+export class DiscoveryFormComponent implements OnInit {
+    @Input() complete: (err: any)=>void;
+    discoveryPath: string;
+    address: string;
+    username: string;
+    password: string;
+
+
+    constructor(@Inject(Angular2InjectionTokens.PLUGIN_DEFINITION) private pluginDefinition: ZLUX.ContainerPluginDefinition,   
+    private graphService: GraphService) {
+        this.discoveryPath = RocketMVD.uriBroker.pluginRESTUri(this.pluginDefinition.getBasePlugin(), 'discovery',"");
+    }
+
+    ngOnInit(): void {
+        //throw new Error("Method not implemented.");
+    }
+
+  //@Input('nodes') nodes: any;
+  //@Input('links') links: any;
+  
+  discover(): void {
+    console.log("Pshhh");
+    this.graphService.discovery(this.discoveryPath, this.address, this.username, this.password, (err: any) => {
+        console.log("Discovery completer with " + err);
+        this.complete(err);
+    });
+  }
+}
